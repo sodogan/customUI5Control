67639c7463ee61a464a5c0c34a36951f157bccbf
sodogan <solendogan.sap@gmail.com>
2019-11-08 18:47:04 +0000

    Add hover function

--- webapp/controller/First.controller.js
+++ webapp/controller/First.controller.js
@@ -1,11 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	'sap/m/MessageBox'
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("myFiori.controller.App", {
 		onInit: function () {
 
+		},
+		onHover: function () {
+			MessageBox.information(
+				"You have clicked the Custom Button.", {}
+			);
 		}
 	});
 });
